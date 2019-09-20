e309e00dfa9be7d30aae563de91b8eff2dfbc3f7
unknown <rafalealpsantos@gmail.com>
2019-09-20 11:13:09 -0300

    First commit

---
+++ Tp_Decriptografia_RafaelLeal/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Tp_Decriptografia_RafaelLeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDecriptografar = new System.Windows.Forms.Button();
+            this.txtCaminhoArquivoCriptografado = new System.Windows.Forms.TextBox();
+            this.txtCaminhoArquivoDecriptografado = new System.Windows.Forms.TextBox();
+            this.txtChave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEscolherArquivoCriptografado = new System.Windows.Forms.Button();
+            this.btnEscolherArquivoDeSaida = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEscolherArquivoCriptografado);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCaminhoArquivoCriptografado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(782, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arquivo a ser Decriptografado";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEscolherArquivoDeSaida);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtCaminhoArquivoDecriptografado);
+            this.groupBox2.Location = new System.Drawing.Point(12, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(782, 91);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arquivo que irá conter o texto  decriptografado";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtChave);
+            this.groupBox3.Location = new System.Drawing.Point(12, 216);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(782, 91);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chave de Decriptografia";
+            // 
+            // btnDecriptografar
+            // 
+            this.btnDecriptografar.Location = new System.Drawing.Point(355, 341);
+            this.btnDecriptografar.Name = "btnDecriptografar";
+            this.btnDecriptografar.Size = new System.Drawing.Size(107, 23);
+            this.btnDecriptografar.TabIndex = 2;
+            this.btnDecriptografar.Text = "Decriptografar";
+            this.btnDecriptografar.UseVisualStyleBackColor = true;
+            this.btnDecriptografar.Click += new System.EventHandler(this.btnDecriptografar_Click);
+            // 
+            // txtCaminhoArquivoCriptografado
+            // 
+            this.txtCaminhoArquivoCriptografado.Location = new System.Drawing.Point(219, 41);
+            this.txtCaminhoArquivoCriptografado.Name = "txtCaminhoArquivoCriptografado";
+            this.txtCaminhoArquivoCriptografado.ReadOnly = true;
+            this.txtCaminhoArquivoCriptografado.Size = new System.Drawing.Size(370, 20);
+            this.txtCaminhoArquivoCriptografado.TabIndex = 0;
+            // 
+            // txtCaminhoArquivoDecriptografado
+            // 
+            this.txtCaminhoArquivoDecriptografado.Location = new System.Drawing.Point(219, 40);
+            this.txtCaminhoArquivoDecriptografado.Name = "txtCaminhoArquivoDecriptografado";
+            this.txtCaminhoArquivoDecriptografado.ReadOnly = true;
+            this.txtCaminhoArquivoDecriptografado.Size = new System.Drawing.Size(370, 20);
+            this.txtCaminhoArquivoDecriptografado.TabIndex = 1;
+            // 
+            // txtChave
+            // 
+            this.txtChave.Location = new System.Drawing.Point(219, 39);
+            this.txtChave.MaxLength = 8;
+            this.txtChave.Name = "txtChave";
+            this.txtChave.Size = new System.Drawing.Size(370, 20);
+            this.txtChave.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Caminho do arquivo decriptografado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Digite a Chave do arquivo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Caminho do Arquivo Criptografado";
+            // 
+            // btnEscolherArquivoCriptografado
+            // 
+            this.btnEscolherArquivoCriptografado.Location = new System.Drawing.Point(634, 39);
+            this.btnEscolherArquivoCriptografado.Name = "btnEscolherArquivoCriptografado";
+            this.btnEscolherArquivoCriptografado.Size = new System.Drawing.Size(107, 23);
+            this.btnEscolherArquivoCriptografado.TabIndex = 3;
+            this.btnEscolherArquivoCriptografado.Text = "Escolher";
+            this.btnEscolherArquivoCriptografado.UseVisualStyleBackColor = true;
+            this.btnEscolherArquivoCriptografado.Click += new System.EventHandler(this.btnEscolherArquivoCriptografado_Click);
+            // 
+            // btnEscolherArquivoDeSaida
+            // 
+            this.btnEscolherArquivoDeSaida.Location = new System.Drawing.Point(634, 36);
+            this.btnEscolherArquivoDeSaida.Name = "btnEscolherArquivoDeSaida";
+            this.btnEscolherArquivoDeSaida.Size = new System.Drawing.Size(107, 23);
+            this.btnEscolherArquivoDeSaida.TabIndex = 5;
+            this.btnEscolherArquivoDeSaida.Text = "Escolher";
+            this.btnEscolherArquivoDeSaida.UseVisualStyleBackColor = true;
+            this.btnEscolherArquivoDeSaida.Click += new System.EventHandler(this.btnEscolherArquivoDeSaida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 392);
+            this.Controls.Add(this.btnDecriptografar);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Decriptografador";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtCaminhoArquivoCriptografado;
+        private System.Windows.Forms.TextBox txtCaminhoArquivoDecriptografado;
+        private System.Windows.Forms.TextBox txtChave;
+        private System.Windows.Forms.Button btnDecriptografar;
+        private System.Windows.Forms.Button btnEscolherArquivoCriptografado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEscolherArquivoDeSaida;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tp_Decriptografia_RafaelLeal/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tp_Decriptografia_RafaelLeal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string arqCriptografado;
+        private string arqDecriptografado;
+
+
+        private void btnEscolherArquivoCriptografado_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selecionarArquivoCriptografado = new OpenFileDialog();
+            selecionarArquivoCriptografado.DefaultExt = "txt";
+            selecionarArquivoCriptografado.InitialDirectory = @"C:\";
+            selecionarArquivoCriptografado.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; ;
+
+            if (selecionarArquivoCriptografado.ShowDialog() == DialogResult.OK)
+            {
+                    arqCriptografado = selecionarArquivoCriptografado.FileName;
+                    txtCaminhoArquivoCriptografado.Text = arqCriptografado;                
+                                                  
+            }
+        }
+
+        private void btnEscolherArquivoDeSaida_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selecionarArquivoDecriptografado = new OpenFileDialog();
+            selecionarArquivoDecriptografado.DefaultExt = "txt";
+            selecionarArquivoDecriptografado.InitialDirectory = @"C:\";
+            selecionarArquivoDecriptografado.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; ;
+
+            if (selecionarArquivoDecriptografado.ShowDialog() == DialogResult.OK)
+            {
+                arqDecriptografado = selecionarArquivoDecriptografado.FileName;
+
+                if (File.Exists(arqDecriptografado))
+                {
+                    arqDecriptografado = selecionarArquivoDecriptografado.FileName;
+                    txtCaminhoArquivoDecriptografado.Text = arqDecriptografado;
+                }
+                else
+                {
+                    File.Create(arqCriptografado);
+                    arqCriptografado = selecionarArquivoDecriptografado.FileName;
+                    txtCaminhoArquivoCriptografado.Text = arqCriptografado;
+                }
+            }
+        }
+
+        private void btnDecriptografar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtChave.Text) && txtChave.Text.Length == 8 && !string.IsNullOrEmpty(arqCriptografado) && !string.IsNullOrEmpty(arqDecriptografado)) {
+
+                try
+                {
+
+                    FileStream le = new FileStream(arqCriptografado, FileMode.Open);
+                    BinaryReader br = new BinaryReader(le);
+
+                    byte[] bytesTexto = br.ReadBytes((int)le.Length);
+                    char[] textoCriptografado = Encoding.UTF8.GetString(bytesTexto).ToCharArray();
+                    char[] textoDecriptografado = new char[textoCriptografado.Length];
+
+                    char[] textoChave = txtChave.Text.ToCharArray();
+                    long valorChave = 0;
+
+                    for (int i = 0; i < textoChave.Length; i++)
+                    {
+                        long valorIndice = textoChave[i];
+                        valorChave += valorIndice;
+                    }
+
+
+                    char[,] subBytes = new char[8, 8] {
+
+                        { '!', '´', 'ç', 'ô', 'A', ',', '~', '[' },
+                        { 'l', 'î', 'Ö', 'B', 'ƒ', 'Ñ', 'Ñ', 'a'},
+                        { '£', 'l', 'Ê', '0', 'ú', 'z', 'æ', '{' },
+                        { '}', '2', 'P', 'z', '|', ']', '%', '&' },
+                        { '$', '*', '<', '.', 'B', 'h', 'o', '?' },
+                        { 'j', '#', '@', 'C', 'v', '6', ':', '-' },
+                        { ';', 'K', 'V', 'û', 'f', 't', 'T', 'R' },
+                        { '=', 'G', 'L', 'S', 'y', 'W', '7', '9' }
+                    };
+
+                    for (int i = 0; i < textoCriptografado.Length;)
+                    {
+
+                        int l = 0;
+                        int c = i;
+
+
+                        while ((c < subBytes.GetLength(0)) && (i < textoCriptografado.Length))
+                        {
+
+                            char valorVetor = subBytes[l, c];
+
+
+                            textoDecriptografado[i] = (char)(textoCriptografado[i] - valorChave - valorVetor);
+
+                            c++;
+                            i++;
+
+                            if (!(c < subBytes.GetLength(0)) && (l < subBytes.GetLength(1)))
+                            {
+                                c = 0;
+                                l++;
+                                i++;
+                            }
+                            if (!(l < subBytes.GetLength(1)))
+                            {
+                                l = 0;
+                                c = 0;
+                            }
+
+                        }
+
+                        FileStream escreve = new FileStream(arqDecriptografado, FileMode.Open);
+                        BinaryWriter bw = new BinaryWriter(escreve);
+
+                        bw.Write(textoDecriptografado);
+                        
+
+                        MessageBox.Show("Decriptografia Bem Sucedida!");
+
+                        le.Close();
+                        escreve.Close();
+                        br.Close();
+                        bw.Close();
+
+                    }
+                }
+                catch
+                {
+                    throw new Exception("Não Foi possível Decriptografar o Arquivo!");
+                }
+                                               
+                
+
+            }
+            else if (string.IsNullOrEmpty(arqCriptografado))
+            {
+
+                MessageBox.Show("Arquivo não escolhido, Selecione  o Arquivo a ser criptografado!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (string.IsNullOrEmpty(arqDecriptografado))
+            {
+
+                MessageBox.Show("Arquivo não escolhido, Selecione  o arquivo de saída ", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (string.IsNullOrEmpty(txtChave.Text))
+            {
+
+                MessageBox.Show(" Chave não pode estar vazia, Digite uma chave. ", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+
+                MessageBox.Show("A chave tem que conter 8 dígitos", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
